039942593d30ffabfd8df0fe43a2cf872db27bd8
SchalterDev <schalter.dev@gmail.com>
2018-01-08 11:18:38 +0100

    Path 210 - 214

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -6,6 +6,16 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  def roll(count)
+    @values = (1..count).map { rand(5) + 1 }
+  end
+
+  def values
+    @values
+  end
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
